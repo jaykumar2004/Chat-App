58208acd61c3082221c2112d742506449365d48c
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-26 18:28:06 +0530

    deployed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,15 +5,18 @@ import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.routes.js";
 import cors from "cors";
-import{app,server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js";
+
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // ✅ Set higher limit for JSON and form data
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(cookieParser());
 
@@ -26,6 +29,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is successfully listening on PORT : ${PORT}`);
