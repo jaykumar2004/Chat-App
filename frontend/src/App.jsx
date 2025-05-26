f16e9bcc3e2ffbd245e350019475489a65d196b0
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-26 15:30:09 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import Homepage from "./pages/HomePage";
 import { useAuthStore } from "./store/useAuthStore";
+import {useThemeStore} from "./store/useThemeStore"
 import { Loader } from "lucide-react";
 
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const {theme} = useThemeStore()
 
   useEffect(() => {
     checkAuth();
@@ -28,7 +30,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
